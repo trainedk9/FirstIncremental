a3ba1da859a28ae6f5383a9b4d52e6a265d547b9
trainedk9 <110599227+trainedk9@users.noreply.github.com>
2023-12-14 08:11:22 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,40 @@
 var gameData = {
     gold: 0,
     goldPerClick: 1,
-    goldPerClickCost: 10
+    goldPerClickCost: 10,
+    minerCost = 50,
+    minerCount = 0,
 }
 
 function mineGold() {
     gameData.gold += gameData.goldPerClick
-    document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
+    document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined (+" + gameData.minerCount + "/s)"
 }
 
 function buyGoldPerClick() {
     if (gameData.gold >= gameData.goldPerClickCost) {
         gameData.gold -= gameData.goldPerClickCost
         gameData.goldPerClick += 1
-        gameData.goldPerClickCost *= 2
-        document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
+        gameData.goldPerClickCost *= 1.5
+        document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined (+" + gameData.minerCount + "/s)"
         document.getElementById("perClickUpgrade").innerHTML = "Upgrade Pickaxe (Currently Level " + gameData.goldPerClick + ") Cost: " + gameData.goldPerClickCost + " Gold"
     }
 }
 
+function buyMiner() {
+    if (gameData.gold >= gameData.minerCost) {
+        gameData.gold -= gameData.minerCost
+        gameData.minerCount += 1
+        gameData.minerCost *= 1.75
+        document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined (+" + gameData.minerCount + "/s)"
+        document.getElementById("minerBuy").innerHTML = "Buy Miner (Currently have " + gameData.minerCount + ") Cost: " + gameData.minerCost + " Gold"
+    }
+}
+
 var mainGameLoop = window.setInterval(function () {
-    mineGold()
+    for (let i = 0; i < minerCount; i++) {
+        mineGold()
+    }
 }, 1000)
 
 var saveGameLoop = window.setInterval(function () {
